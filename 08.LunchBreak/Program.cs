bfec8bb36399ba047da07c9834b1bce41f3d5db6
Evgeni Vladimirov <evgeni_vlad@abv.bg>
2024-01-14 17:54:05 +0200

    hghh

---
+++ 08.LunchBreak/Program.cs
@@ -0,0 +1,19 @@
+﻿string serial = Console.ReadLine();
+int timeEpisode = int.Parse(Console.ReadLine());
+int timeBrake = int.Parse(Console.ReadLine());
+double timeForLunch = timeBrake / 8.0;
+double timeForBrake = timeBrake / 4.0;
+
+
+double neededTime = timeEpisode + timeForLunch + timeForBrake;
+double diffTime = Math.Abs(neededTime - timeBrake);
+
+if (neededTime <= timeBrake)
+{
+    Console.WriteLine($"You have enough time to watch {serial} and left with {Math.Ceiling(diffTime)} minutes free time.");
+}
+
+else
+{
+    Console.WriteLine($"You don't have enough time to watch {serial}, you need {Math.Ceiling(diffTime)} more minutes.");
+}
