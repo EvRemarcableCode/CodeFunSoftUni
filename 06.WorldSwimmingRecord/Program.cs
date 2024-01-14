772f4931bee36407bfeec9cd8dd9b7eb52980d46
Evgeni Vladimirov <evgeni_vlad@abv.bg>
2024-01-14 11:50:31 +0200

    #NewCode

---
+++ 06.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,16 @@
+﻿double seconds = double.Parse (Console.ReadLine ());
+double meters = double.Parse (Console.ReadLine ());
+double time = double.Parse (Console.ReadLine ());
+double totalTime = meters * time;
+double late= Math.Floor(meters / 15) * 12.5;
+double totalIvan = late + totalTime;
+double timeIvan = totalIvan - seconds;
+
+if (totalIvan >= seconds)
+{
+    Console.WriteLine($"No, he failed! He was {timeIvan:F2} seconds slower.");
+}
+else
+{
+    Console.WriteLine($"Yes, he succeeded! The new world record is {totalIvan:F2} seconds.");
+}
