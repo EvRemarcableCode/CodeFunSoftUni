f88e8cc5c7d9203024507530c964af5be15898ff
Evgeni Vladimirov <evgeni_vlad@abv.bg>
2024-01-14 17:16:45 +0200

    ууу

---
+++ 07.Shopping/Program.cs
@@ -0,0 +1,23 @@
+﻿double userBudget = double.Parse(Console.ReadLine());
+int videoCard = int.Parse(Console.ReadLine());
+int procesor = int.Parse(Console.ReadLine());
+int ram = int.Parse(Console.ReadLine());
+var discount = 0.15;
+double videoCardPrice = 250 * videoCard;
+double cpuPrice = videoCardPrice * 0.35 * procesor;
+double ramPrice = videoCardPrice * 0.10 * ram;
+double totalPrice = videoCardPrice + cpuPrice + ramPrice;
+
+if (videoCardPrice > cpuPrice)
+{
+    totalPrice = totalPrice * discount;
+}
+
+if (userBudget >= totalPrice)
+{
+    Console.WriteLine($"You have {userBudget - totalPrice:F2} leva left!");
+}
+else
+{
+    Console.WriteLine($"Not enough money! You need {totalPrice - userBudget:F2} leva more!");
+}
---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿double budget = double.Parse(Console.ReadLine());
+int videoCards = int.Parse(Console.ReadLine());
+int processors = int.Parse(Console.ReadLine());
+int ramMemory = int.Parse(Console.ReadLine());
+double videoCardPrice = 250 * videoCards;
+double processorPrice = (videoCardPrice * 0.35) * processors;
+double ramMemoryPrice = (0.1 * videoCardPrice) * ramMemory;
+double totalPrice = videoCardPrice + processorPrice + ramMemoryPrice;
+
+if (videoCards > processors)
+{
+    totalPrice -= totalPrice * 0.15;
+}
+if (budget >= totalPrice)
+{
+    Console.WriteLine($"You have {budget - totalPrice:F2} leva left!");
+}
+
+else
+{
+    Console.WriteLine($"Not enough money! You need {totalPrice - budget:F2} leva more!");
+}
